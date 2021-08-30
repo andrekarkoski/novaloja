b328e0edf8991f5f0aa53e4ae0f4180f272cc986
andrekarkoski <andreskarkoski@gmail.com>
2021-08-30 15:10:11 -0300

    ajustando postgre

--- novaloja/settings.py
+++ novaloja/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'novaloja.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_novaloja',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
+        'NAME': 'd9qsbdsjaghrtd',
+        'USER': 'ewqryngxsqzazf',
+        'PASSWORD': '09bbc7c19fea7db6e197aeb6fde78a29eede870c01595198ba33d373c83361cd',
+        'HOST': 'postgres://ewqryngxsqzazf:09bbc7c19fea7db6e197aeb6fde78a29eede870c01595198ba33d373c83361cd@ec2-35-153-91-18.compute-1.amazonaws.com:5432/d9qsbdsjaghrtd',
         'PORT': '5432',
     }
 }
